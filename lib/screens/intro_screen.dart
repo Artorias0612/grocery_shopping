776e9ec2a17c7c1dcf25ac0950b458d16e99d5f2
Artorias0612 <mohammadsobhankhandan@gmail.com>
2022-11-25 04:08:15 -0800

    developed intro_screen & linked it to home_screen

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_shopping/screens/home_screen.dart';
 
-class IntroScreen extends StatefulWidget {
+class IntroScreen extends StatelessWidget {
   const IntroScreen({Key? key}) : super(key: key);
 
   @override
-  State<IntroScreen> createState() => _IntroScreenState();
-}
-
-class _IntroScreenState extends State<IntroScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          // logo
+
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 80.0, right: 80.0, bottom: 40.0, top: 40.0),
+            child: Image.asset('assets/images/avocado.png'),
+          ),
+
+          // text
+
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Text(
+              "we deliver groceries at your at your doorstep",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          // Fresh items every day
+
+          Text(
+            'Fresh items everyday',
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+
+          const Spacer(),
+
+          // get started button
+
+          GestureDetector(
+            onTap: () => Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const HomeScreen())),
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(12)),
+              child: const Text(
+                'Get Started',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+
+          const Spacer(),
+        ],
+      ),
+    );
   }
 }
