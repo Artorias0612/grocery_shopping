0991236347ce3c3dd9099861cf19e3664f457a15
Artorias0612 <mohammadsobhankhandan@gmail.com>
2022-12-01 02:47:40 -0800

    added some new shop items to _shopItems Variable

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -5,9 +5,15 @@ class CartModel extends ChangeNotifier {
 
   final List _shopItem = [
     // [itemName, itemPrice, imagePath, color]
-    ["Avocado", "4.00", "assets/images/avocado.png"],
-    ["Banana", "5.00", "assets/images/banana.png"],
-    ["Chicked", "30.00", "assets/images/chicken.png"],
-    ["Water", "2.00", "assets/images/banana.png"],
+    ["Avocado", "4.00", "assets/images/avocado.png", Colors.green],
+    ["Banana", "5.00", "assets/images/banana.png", Colors.yellow],
+    ["Chicked", "30.00", "assets/images/chicken.png", Colors.brown],
+    ["Water", "2.00", "assets/images/water.png", Colors.blue],
+    ["Avocado", "4.00", "assets/images/avocado.png", Colors.green],
+    ["Banana", "5.00", "assets/images/banana.png", Colors.yellow],
+    ["Chicked", "30.00", "assets/images/chicken.png", Colors.brown],
+    ["Water", "2.00", "assets/images/water.png", Colors.blue],
   ];
+
+  get shopItems => _shopItem;
 }
