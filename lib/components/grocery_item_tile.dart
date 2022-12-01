de651eb780a76096b426ba4ba67d681f859adf3a
Artorias0612 <mohammadsobhankhandan@gmail.com>
2022-12-01 02:46:31 -0800

    Compeleted grocery_items Design

---
+++ lib/components/grocery_item_tile.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+import 'package:flutter/material.dart';
+
+class GroceryItemTile extends StatelessWidget {
+  final String itemName;
+  final String itemPrice;
+  final String imagePath;
+  final color;
+
+  const GroceryItemTile({
+    Key? key,
+    required this.itemName,
+    required this.itemPrice,
+    required this.imagePath,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        color: color[100],
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 15,
+          ),
+          // imagePath
+          Image.asset(
+            imagePath,
+            height: 70,
+          ),
+          // itemName
+          Text(itemName),
+
+          // itemPrice
+
+          const SizedBox(
+            height: 30.0,
+          ),
+
+          MaterialButton(
+            onPressed: () {},
+            color: color,
+            child: Text(
+              "\$$itemPrice",
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
